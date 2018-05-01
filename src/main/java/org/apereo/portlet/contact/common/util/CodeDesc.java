353e15a3441545a713b0df5069b432ccbd1eac18
Benito J. Gonzalez <bgonzalez@unicon.net>
2018-05-01 10:57:45 -0700

    Fix CodeDesc mapping

--- src/main/java/org/apereo/portlet/contact/common/util/CodeDesc.java
+++ src/main/java/org/apereo/portlet/contact/common/util/CodeDesc.java
@@ -1,11 +1,15 @@
 package org.apereo.portlet.contact.common.util;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.Value;
 
-@Value
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CodeDesc {
 
